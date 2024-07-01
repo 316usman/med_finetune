79a23683cd3e92b989d1ec3e9a87c84fc7085467
Usman Siddiqui <107467685+316usman@users.noreply.github.com>
2024-07-01 17:28:08 +0500

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -34,7 +34,6 @@ tokenizer = AutoTokenizer.from_pretrained('microsoft/Phi-3-mini-4k-instruct')
 config = LoraConfig(
     r=16,
     lora_alpha=32,
-    target_modules=["q_proj", "v_proj"],
     lora_dropout=0.05,
     bias="none",
     task_type="CAUSAL_LM",
